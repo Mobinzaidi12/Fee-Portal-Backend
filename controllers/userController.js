96b9d961f722318998cf2d8772d3b69f0b0c0f3b
Mobinzaidi12 <mobinzaidi2015@gmail.com>
2023-04-18 23:35:52 +0500

    msg

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const registerUser = async (req, res) => {
   let user = await new User(req.body);
   let result = user.save();
   res.send(user);
+  console.log("mobin");
 };
 
 module.exports = { registerUser };
